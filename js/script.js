a943149ee16985c6ce938b818f5e7cc0700b202b
Mattia Pedone <mattiapedone93@gmail.com>
2020-02-06 11:53:59 +0100

    aggiunta del link ai font

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
 $(document).ready(function() {
-  alert('Ciao porco!:D')
+  alert('Ciao porco!:D');
+
+
+  // $.ajax(
+  //   {
+  //     url: 'https://flynn.boolean.careers/exercises/api/holidays',
+  //     method: 'GET',
+  //     data: {
+  //       year: month.year(),
+  //       month: month.month()
+  //     },
+  //     success: function (data) {
+  //       var holidays = data.response;
+  //       for (var i = 0; i < holidays.length; i++) {
+  //         var thisHoliday = holidays[i];
+  //         var thisHolidayData = thisHoliday.date;
+  //         $('li[data-date-complete="'+ thisHolidayData  +'"]').addClass('holiday');
+  //         $('li[data-date-complete="'+ thisHolidayData  +'"]').find('.nome-festivita').append('-' +  ' ' + thisHoliday.name);
+  //       }
+  //     },
+  //     error: function () {
+  //       alert('errore');
+  //     }
+  //   }
+  // );
 });
