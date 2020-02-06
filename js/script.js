5cb9f6661ab15224a98541ce9c63c8e8fecd38bc
Mattia Pedone <mattiapedone93@gmail.com>
2020-02-06 16:24:55 +0100

    ricerca film completata

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
 $(document).ready(function() {
   var url = 'https://api.themoviedb.org/3/search/movie';
-  var query = 'Il gladiatore';
-  // var userInput = $('input').val();
-  // console.log(userInput);
-  $.ajax(
-    {
-      url: url,
-      method: 'GET',
-      data: {
-        api_key: 'c0810927127de0abbc728e88cbc79828',
-        query: query
-      },
-      success: function (data) {
-        var movies = data.results;
-        printSingleMovie(movies);
-      },
-      error: function (request, state, errors) {
-        alert('errore');
+  $(document).on('click', '.search_btn', function () {
+    var text = $('.search_movies').val().toLowerCase();
+    $.ajax(
+      {
+        url: url,
+        method: 'GET',
+        data: {
+          api_key: 'c0810927127de0abbc728e88cbc79828',
+          query: text
+        },
+        success: function (data) {
+          var movies = data.results;
+          printSingleMovie(movies);
+        },
+        error: function (request, state, errors) {
+          alert('errore');
+        }
       }
-    }
-  );
+    );
+  });
 });
 
 function printSingleMovie(array) {
@@ -37,15 +37,3 @@ function printSingleMovie(array) {
     $('.movies_list_container').append(html);
   }
 }
-// handlebars
-// var source = document.getElementById("entry-template").innerHTML;
-// var template = Handlebars.compile(source);
-// var context = { title: "My New Post", body: "This is my first post!" };
-// var html = template(context);
-
-
-
-
-
-// MY API KEY
-// c0810927127de0abbc728e88cbc79828
