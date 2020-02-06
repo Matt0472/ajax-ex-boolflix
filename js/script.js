e2b4399b2c421c38d2608c38e5b08e1552411d33
Mattia Pedone <mattiapedone93@gmail.com>
2020-02-06 16:05:13 +0100

    aggiunta function stampa film

--- js/script.js
+++ js/script.js
@@ -13,6 +13,8 @@ $(document).ready(function() {
       },
       success: function (data) {
         console.log(data);
+        var films = data.results;
+        console.log(films);
       },
       error: function (request, state, errors) {
         alert('errore');
@@ -21,7 +23,21 @@ $(document).ready(function() {
   );
 });
 
-
+function printSingleFilm(array) {
+  var source = $('#entry-template').html();
+  var template = Handlebars.compile(source);
+  for (var i = 0; i < array.length; i++) {
+    var thisFilm = array[i];
+    var context = {
+      title: thisFilm.title,
+      original_title: thisFilm.original_title,
+      original_language: thisFilm.original_language,
+      vote_average: thisFilm.vote_average
+    };
+    var html = template(context);
+    $('.movies_list_container').append(html);
+  }
+}
 // handlebars
 // var source = document.getElementById("entry-template").innerHTML;
 // var template = Handlebars.compile(source);
